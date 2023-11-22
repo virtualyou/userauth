2270ba5cec5147b7e2a127a2089ed7d122e7d13b
David L Whitehurst <me@dlwhitehurst.com>
2023-11-21 20:42:46 -0500

    set port to 3001

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
