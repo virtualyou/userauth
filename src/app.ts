a1a90b1f69251f97e25e4f4b527255dfa7712672
David L Whitehurst <me@dlwhitehurst.com>
2024-03-13 14:45:20 -0400

    v0.1.19-prod

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ app.use(
     cookieSession({
       name: "virtualyou-session",
       keys: ["COOKIE_SECRET"],
-      domain: '.virtualyou.info',
+      domain: '.virtualyouplan.com',
       httpOnly: true,
       sameSite: 'strict'
     })
