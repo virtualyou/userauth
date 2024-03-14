493c6af4a5e3fac6cc79dbb8699392e3cb2aae72
David L Whitehurst <me@dlwhitehurst.com>
2024-03-14 17:26:51 -0400

    added swagger to all

--- src/app.ts
+++ src/app.ts
@@ -27,14 +27,15 @@ import userRouter from "./routes/user.routes";
 import cookieSession from "cookie-session";
 import * as process from "process";
 
+// swagger api documentation
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+
 const initIndex = process.argv.indexOf("--init=true");
 const init = initIndex !== -1;
 
 const app: Express = express();
 
-// swagger api documentation
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json";
 
 dotenv.config();
 
